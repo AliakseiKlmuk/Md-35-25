fcdddd107b0e6415660615a36f93660b56c56e17
AlexKlimuk <Lechiy@tut.by>
2026-01-04 11:03:58 +0300

    create module_3.1

---
+++ module_3.1/module_3.1/module_3.1.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm> // Для std::transform
+#include <cctype>    // Для tolower/toupper
+
+enum class TrafficLight { UNKNOWN = -1,
+                          RED, 
+                          YELLOW, 
+                          GREEN };
+
+void printLight(TrafficLight color) {
+
+    switch (color) {
+    case TrafficLight::RED:
+        std::cout << "Red" << std::endl;
+        break;
+    case TrafficLight::YELLOW:
+        std::cout << "Yellow" << std::endl;
+        break;
+    case TrafficLight::GREEN:
+        std::cout << "Green" << std::endl;
+        break;
+    default:
+        std::cout << "Unknown color" << std::endl;
+    }
+}
+
+int main()
+{
+    TrafficLight light;
+    light = TrafficLight::RED;
+    printLight(light);
+
+    std::cout << "Enter color Red, Yellow, Green: " << std::endl;
+
+    std::string color;
+    std::cin >> color;
+    std::transform(color.begin(), color.end(), color.begin(), ::tolower);
+
+    if (color == "red") {
+        printLight(TrafficLight::RED);
+    }
+    else if (color == "yellow") {
+        printLight(TrafficLight::YELLOW);
+    }
+    else if (color == "green") {
+        printLight(TrafficLight::GREEN);
+    }
+    else {
+        printLight(TrafficLight::UNKNOWN);
+    }
+}
